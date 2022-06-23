21c6cdbf1bb4c2e13b0b772d8da24a14e9aac1f6
MannanovIN <79753411+MannanovIN@users.noreply.github.com>
2022-06-23 16:01:28 +0300

    Create update_starknet.sh

---
+++ StarkNet/update_starknet.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "-----------------------------------------------------------------------------"
+echo "Начинаем обновление репрозитория "
+echo "-----------------------------------------------------------------------------"
+cd ~/pathfinder/py
+git fetch &>/dev/null
+git checkout v0.2.2-alpha &>/dev/null
+echo "Репозиторий успешно обновлен, начинаем билд"
+echo "-----------------------------------------------------------------------------"
+python3 -m venv .venv &>/dev/null
+source .venv/bin/activate &>/dev/null
+PIP_REQUIRE_VIRTUALENV=true pip install --upgrade pip &>/dev/null
+PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt &>/dev/null
+cargo build --release --bin pathfinder &>/dev/null
+sleep 2
+source $HOME/.bash_profile &>/dev/null
+echo "Билд завершен успешно"
+echo "-----------------------------------------------------------------------------"
+systemctl restart starknet
+pathfinder -V
+echo "-----------------------------------------------------------------------------"
